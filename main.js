923c568ca683e0501b00b74897cee8ceea98b47d
Fiona Weaver <fiona.l.weaver@gmail.com>
2021-03-02 14:00:16 -0700

    commit tree 85e837fdc6b076d68dfd1e2df47f2424fab57737

--- main.js
+++ main.js
@@ -3,7 +3,25 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.getElementById('modal');
+const likeButtons = document.getElementsByClassName('like-glyph');
 
+modal.className = "hidden"
+
+for (const heart of likeButtons) {
+  heart.addEventListener('click', () => {
+    mimicServerCall()
+      .then(() => {
+        heart.className = "activated-heart"
+      })
+      .catch(() => {
+        modal.style.visibility = "visible"
+      })
+  })
+}
+document.addEventListener('DOMContentLoaded', function () {
+
+})
 
 
 
@@ -11,9 +29,9 @@ const FULL_HEART = '♥'
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
